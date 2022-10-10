392b081e917ec84534c6784f53ba3f731670a914
tatsuya-ogi312 <112700622+tatsuya-ogi312@users.noreply.github.com>
2022-10-10 21:36:56 +0900

    ユーザー管理機能/Basic認証実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_origin
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+      keys: [:nickname, :age, :sex_id, :height, :weight])
+  end
+
+  def basic_origin
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_ORIGIN_USER'] && password == ENV['BASIC_ORIGIN_PASSWORD']
+    end
+  end
+
 end
