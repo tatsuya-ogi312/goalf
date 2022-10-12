bf7febb2d1003209082bfa41ebb02df1406f67fa
tatsuya-ogi312 <112700622+tatsuya-ogi312@users.noreply.github.com>
2022-10-12 18:38:37 +0900

    記事関連の機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
+ before_action :authenticate_user!, except: [:index, :show]
+ before_action :find_article, only: [:show, :edit, :update, :destroy]
+ before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def index
-    @articles = Article.includes(:user)
+    @articles = Article.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -16,9 +20,36 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to article_path(@article.id)
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :image, :content, :url, :category_id).merge(user_id: current_user.id)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path if @article.user_id != current_user.id
   end
 
 end
