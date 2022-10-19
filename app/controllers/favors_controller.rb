53534ba6dd0a09cf278af4c80fde70f2f37d2efd
tatsuya-ogi312 <112700622+tatsuya-ogi312@users.noreply.github.com>
2022-10-19 12:02:32 +0900

    清書＋必要機能全実装

--- app/controllers/favors_controller.rb
+++ app/controllers/favors_controller.rb
@@ -1,6 +1,12 @@
 class FavorsController < ApplicationController
-  before_action :article_params
-  before_action :authenticate_user!
+  before_action :article_params, except: :index
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @user = User.find(params[:user_id])
+    favor = Favor.where(user_id: @user.id).pluck(:article_id) #plunkでarticle_idを全て取得
+    @favor_articles = Article.find(favor)
+  end
 
   def create
     Favor.create(user_id: current_user.id, article_id: params[:id])
